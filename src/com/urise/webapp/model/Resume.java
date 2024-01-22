1820d7fde77b95fd42efcdcad964718caca723de
Oksana Dzhioeva <km@lume21.ru>
2024-01-22 20:03:18 +0300

    HW-2: added packages and methods

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,10 +1,20 @@
+package com.urise.webapp.model;
+
 /**
  * Initial resume class
  */
 public class Resume {
-
     // Unique identifier
     String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
 
     @Override
     public String toString() {
